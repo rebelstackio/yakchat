ddc4478f8560b53d0bc14f2ae4523873ed179400
Osmar Reyes <osmarreyesst@gmail.com>
2019-03-03 01:08:30 -0500

    use parcel to generate the dev dependencies

--- front-script/dist/main.e54e2ae9.js
+++ front-script/dist/main.e54e2ae9.js
@@ -69682,7 +69682,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36199" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40291" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
