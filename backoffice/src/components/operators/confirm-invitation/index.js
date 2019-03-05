956bd0c1de93f9f21205e0525071ebb1e5eaa10a
Pablo.Limo <pablo.limo@dealerbuilt.com>
2019-03-05 15:52:30 -0500

    created form for operator validation

---
+++ backoffice/src/components/operators/confirm-invitation/index.js
@@ -0,0 +1,36 @@
+import { MetaComponent } from '@rebelstack-io/metaflux';
+import './style.css';
+
+class ConfirmInvitation extends MetaComponent {
+	constructor () {
+		super(global.storage);
+	}
+	render () {
+		const content = document.createElement('div');
+		content.className = 'confirm-invitation-container';
+		content.innerHTML = `
+			<div class="form-header">
+				<div>Title</div>
+			</div>
+			<div class="form-body">
+				<div><input type="text" placeholder="Username"/></div>
+				<div><input type="text" placeholder="Email"/></div>
+				<div><input type="password" placeholder="Password"/></div>
+			</div>
+			<div class="form-footer">
+				<div>Save</div>
+			</div>
+		`;
+		return content;
+	}
+	handleStoreEvents () {
+		return {
+			'*': () => {
+				//.
+			}
+		}
+	}
+	addListeners () {
+		//.
+	}
+}
