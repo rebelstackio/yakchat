4b3c4a22f58d5d5d286de11a6c646d61a065e6e4
Javier Galarza <jegj57@gmail.com>
2019-03-22 10:57:39 -0500

    Remove  deprecated staments in workbox

--- backoffice/src/firebase-messaging-sw.js
+++ backoffice/src/firebase-messaging-sw.js
@@ -1,10 +1,5 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 importScripts('https://www.gstatic.com/firebasejs/5.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.7.2/firebase-messaging.js');
-
-workbox.core.setCacheNameDetails({ prefix: 'yakchat-cache-v1' });
-
-workbox.precaching.suppressWarnings();
 
 // Placeholder array which is populated automatically by workboxBuild.injectManifest()
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
