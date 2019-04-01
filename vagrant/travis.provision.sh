837677306660840191f86407fd655aa3c59cb1ba
Javier Galarza <jegj57@gmail.com>
2019-04-01 08:16:54 -0500

    Add conditial for travis and vagrant

--- vagrant/travis.provision.sh
+++ vagrant/travis.provision.sh
@@ -2,12 +2,12 @@
 travis_arg=$1
 set -a
 LOG=${LOG:-/home/vagrant/yakchat/vagrant/tmp/log/boot.log}
+VAGRANT_PROVISION=${VAGRANT_PROVISION:-0}
 t_pull_request=${travis_arg:-"$TRAVIS_PULL_REQUEST"}
 set +a
 
 
 # echo "Create AWS Credentials file..."
-
 # sudo mkdir ~/.aws
 # cat <<EOF > ~/.aws/credentials
 # aws_access_key_id = $AWS_ACCESS_KEY_ID
@@ -18,7 +18,10 @@ set +a
 # echo "AWS_ACCESS_KEY_ID-------->$AWS_ACCESS_KEY_ID"
 # echo "AWS_SECRET_ACCESS_KEY-------->$AWS_SECRET_ACCESS_KEY"
 
-echo "Installing Python and PIP"
+if [[ $VAGRANT_PROVISION -eq 1 ]]; then
+  echo "Installing Python and PIP"
+  sudo apt-get update
+fi
 
 # sudo apt-get -y install software-properties-common
 # sudo add-apt-repository ppa:deadsnakes/ppa
@@ -27,6 +30,7 @@ echo "Installing Python and PIP"
 # sudo apt-get -y install python3-pip
 # pip3 install awscli --upgrade --user
 
+echo "..........Creating Pull Request Deploy.........."
 echo "Create a new S3 bucket"
 
 aws s3api create-bucket --bucket "yakchat_$TRAVIS_PULL_REQUEST" --acl public read --region eu-west-1
